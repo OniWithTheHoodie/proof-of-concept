1462dc8387bd6cc6d33d2c9806010f00453a5e8e
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-06-13 14:28:03 +0200

    push
    -added a grid to tablet/desktop
    - filtered variables with the proper id
    - updated navbar
    - added a button
    - added searchbar

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ import fetchJson from './helpers/fetch-json.js'
 // Haal ik de Url vooor fdnd-agency in Directus op en zet ik het in een variable genaamd apiURL
 const apiUrl = 'https://fdnd-agency.directus.app/'
 // Haal ik deloitte prompts tabel op en voeg ik samen met apirURL variable tot 1 URL waar de variable deloittePrompt word
-const deloittePrompt = await fetchJson(apiUrl + 'items/deloitte_prompts')
+const deloitteFinancial = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=1')
+const deloitteCompliance = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=2')
+const deloitteDevelopment = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=3')
+const deloittePlanning = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=4')
+const deloitteResearch = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=5')
+const deloitteProcessing = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=6')
+const deloitteMeetings = await fetchJson(apiUrl + 'items/deloitte_prompts?filter[id][_eq]=7')
 // hier log ik om te kijken of de data wel goed word opgehaald door de deloittePrompt te loggen in de terminal krijg ik data terug gestuurd
-console.log(deloittePrompt)
+console.log(deloitteResearch)
 
 // hier maak ik een nieuwe express app aan
 const app = express()
@@ -31,10 +37,95 @@ app.use(express.urlencoded({extended: true}))
 
 // Hier haal ik de root op dat is index.ejs waar een functie word gemaakt en vraag het op en wacht op een reactie en daarna word de URL in deloittePrompt gefetched
 app.get('/', function(request, response){
-    fetchJson(deloittePrompt)
     // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
-    response.render('index', {
-        allPrompts: deloittePrompt.data
+    response.render('index')
+})
+
+// Route to audit.ejs
+app.get('/audit', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('audit')
+})
+
+// Route to consulting.ejs
+app.get('/consulting', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('consulting')
+})
+
+// Route to innovation.ejs
+app.get('/innovation', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('innovation')
+})
+
+// Route to financialReview.ejs
+app.get('/financialReview', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloitteFinancial)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('financialReview', {
+        allPrompts: deloitteFinancial.data
+    })
+})
+
+// Route to complianceCheck.ejs
+app.get('/complianceCheck', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloitteCompliance)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('complianceCheck', {
+        allPrompts: deloitteCompliance.data
+    })
+})
+
+// Route to productDevelopment.ejs
+app.get('/productDevelopment', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloitteDevelopment)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('productDevelopment', {
+        allPrompts: deloitteDevelopment.data
+    })
+})
+
+// Route to productLaunchPlanning.ejs
+app.get('/productLaunchPlanning', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloittePlanning)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('productLaunchPlanning', {
+        allPrompts: deloittePlanning.data
+    })
+})
+
+// Route to marketResearch.ejs
+app.get('/marketResearch', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloitteResearch)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('marketResearch', {
+        allPrompts: deloitteResearch.data
+    })
+})
+
+// Route to productLaunchPlanning.ejs
+app.get('/documentProcessing', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloitteProcessing)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('documentProcessing', {
+        allPrompts: deloitteProcessing.data
+    })
+})
+
+// Route to marketResearch.ejs
+app.get('/clientMeetings', function(request, response){
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    fetchJson(deloitteMeetings)
+    // hier wordt de reactie gerenderd tot index.ejs en geef ik mee de data deloittePrompt
+    response.render('clientMeetings', {
+        allPrompts: deloitteMeetings.data
     })
 })
 
